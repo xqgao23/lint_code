bf2c993120d7b733b3d0ccd8daf4d6cb36de6523
xqgao <xqgao23@gmail.com>
2015-12-01 11:50:27 +0800

    add anagrams.py

---
+++ usa_company/anagrams.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def anagram(self, strs):
+        pass
+
+if __name__ == "__main__":
+    solution = Solution()
+    if solution.anagram(["lint","intl","inlt","code"] == ["lint","inlt","intl"]:
+        print('pass')
