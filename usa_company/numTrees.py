cb19a824a1df62c950a98ca0de2d17b20c7fe2ce
xqgao <xqgao23@gmail.com>
2017-01-10 11:39:20 +0800

    numTrees.py

--- usa_company/numTrees.py
+++ usa_company/numTrees.py
@@ -5,9 +5,37 @@ class Solution:
     # @param n: An integer
     # @return: An integer
     def numTrees(self, n):
-        pass
+        if n == 0:
+            return 1
+
+        numArray = [x + 1 for x in range(n)]
+        self.max_trees = {}
+        return self.get_max_trees(numArray)
+        
+    def get_max_trees(self, numArray):
+        if len(numArray) < 3:
+            return len(numArray)
+        
+        array_str = str(numArray)
+        if array_str in self.max_trees:
+            return self.max_trees[array_str]
+
+        total_counts = 0
+        for i in range(len(numArray)):
+            left_counts = self.get_max_trees(numArray[:i])
+            right_counts = self.get_max_trees(numArray[i + 1:])
+            if left_counts == 0:
+                left_counts = 1
+            if right_counts == 0:
+                right_counts = 1
+            
+            total_counts = left_counts * right_counts + total_counts
+        self.max_trees[array_str] = total_counts
+        return total_counts
 
 if __name__ == '__main__':
     solution = Solution()
-    if solution.numTrees(3) == 5:
+    if solution.numTrees(0) == 1:
         print('test 1 pass')
+    if solution.numTrees(15) == 9694845:
+        print('test 2 pass')
